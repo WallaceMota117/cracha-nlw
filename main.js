97b5d2b1b84753c40759f52b3c1f46c09b6aea15
Wallace Mota <93005620+WallaceMota117@users.noreply.github.com>
2021-10-26 02:47:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+  github: 'WallaceMota117',
+  youtube: 'wallace carvalho',
+  facebook: 'Gabriel Fernando',
+  instagram: 'wallacedecarvalhomota',
+  twitter: 'ELGabriel'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(Response => Response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGitHubProfileInfos()
